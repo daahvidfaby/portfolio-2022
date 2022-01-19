074ca969286216015689a141b59525dbe6ecc98e
David <david.faby@adeliom.com>
2022-01-20 00:24:16 +0100

    First commit with minimal configuration

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+    <header>
+      David Faby
+
+      <nav>
+        <ul>
+          <li>FR</li>
+          <li>EN</li>
+        </ul>
+      </nav>
+    </header>
+
+    
+  )
+}
+
+export default App
