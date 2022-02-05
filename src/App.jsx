b5ee62fc873492a510c1cbc6bff602f9650c17fe
David <david@Davids-MacBook-Air.local>
2022-02-05 18:49:14 +0100

    Added basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
 
   return (
+    <>
     <header>
       David Faby
 
@@ -12,11 +13,33 @@ function App() {
       </nav>
     </header>
 
-    
+    <section className="hero">
+
+    </section>
+
+    <section className="introduction">
+
+    </section>
+
+    <section className="consulting">
+
+      <div className="consulting__content"></div>
+      <div className="consulting__skills"></div>
+
+    </section>
+
+    <section className="project-typology">
+
+    </section>
+
+    <section className="contact">
+
+    </section>
 
 
+    <footer></footer>
 
-    
+    </>
   )
 }
 
